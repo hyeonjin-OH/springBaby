0c113921968f25b9d4ea47cf3b34498ec387b5d4
hyeonjin-OH <mion0602@gmail.com>
2023-05-15 11:35:41 +0900

    Initial commit

---
+++ src/main/java/inflearn/spring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package inflearn.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello"; //template  밑에 있는 hello.html을 실행하는 것임
+    }
+}
